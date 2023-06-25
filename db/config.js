888e8d983f7114f5894ea3e94f8f6f6a06621f91
preetkumar012 <pksiet84@gmail.com>
2023-06-25 23:01:32 +0530

    first commit

--- db/config.js
+++ db/config.js
@@ -1,8 +1,9 @@
-
+ "use strict"
 const mongoose=require("mongoose");
 
 const DB="mongodb+srv://jewar0123:Avnish1234@cluster0.gcfs3q4.mongodb.net/preet?retryWrites=true&w=majority"
 // console.log(typeof(DB));
+// let DB=process.env.database;
 mongoose.connect(DB).then(()=>{
         console.log("database connected");
     }).catch((err)=>{
