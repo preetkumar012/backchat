ca84d33b6912e702b6415c47dbd7344bf48017d8
preetkumar012 <pksiet84@gmail.com>
2023-06-25 22:23:21 +0530

    first commit

--- db/config.js
+++ db/config.js
@@ -1,2 +1,10 @@
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/preet');
+
+const mongoose=require("mongoose");
+
+const DB="mongodb+srv://jewar0123:<Pyare@123>@cluster0.gcfs3q4.mongodb.net/preet?retryWrites=true&w=majority"
+console.log(typeof(DB));
+mongoose.connect("mongodb+srv://jewar0123:Avnish1234@cluster0.gcfs3q4.mongodb.net/preet?retryWrites=true&w=majority").then(()=>{
+        console.log("database connected");
+    }).catch((err)=>{
+console.log(err);
+    })
